c12817a1d9c4ab7c6e2cbe12a445b0e6c16768d4
shion <m252805@st.tohtech.ac.jp>
2026-01-27 00:42:11 +0900

    feat/ MVP0 終了時刻（HH:MM）を日付またぎ考慮でtimestampに変換する処理を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,26 @@ export default function Page() {
     setEndTime("");
   }
 
+  const toEndTimestamp = (timeHHMM: string) => {
+    const [hh, mm] = timeHHMM.split(":").map(Number)
+
+    const now = new Date();
+    const end = new Date();
+
+    end.setHours(hh, mm, 0, 0);
+
+    if (end.getTime() <= now.getTime()) {
+      end.setDate(end.getDate() + 1);
+    }
+
+    // デバッグ用
+    console.log(end.getFullYear());
+    console.log(end.toLocaleDateString().slice(5));
+    console.log(end.toLocaleTimeString().slice(0, -3));
+
+    return end.getTime();
+  };
+
   return (
     <main className="min-h-screen bg-neutral-50 text-neutral-900">
       <div className="mx-auto max-w-xl px-4 py-10">
@@ -59,7 +79,7 @@ export default function Page() {
             <button
               className="rounded-lg border px-4 py-2 disabled:opacity-40"
               onClick={onReset}
-              disabled={isRunning}
+              disabled={isRunning || !taskName || !endTime}
               title="カウントダウンリセットボタン"
             >
               Reset
@@ -83,6 +103,7 @@ export default function Page() {
           <div className="mt-3 text-xs text-neutral-500">
             状態：{isRunning ? "実行中" : "未開始"}
           </div>
+          {toEndTimestamp("00:20")}
 
 
         </section>
@@ -90,3 +111,4 @@ export default function Page() {
     </main>
   );
 }
+
